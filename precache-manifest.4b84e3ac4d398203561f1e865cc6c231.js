6304a94e4a6ed5f1e09d826506c4b549c69c965f
Tae Hee Choi <tchoi@nitinol.internal>
2020-07-10 08:08:28 -0700

    Updates

---
+++ precache-manifest.4b84e3ac4d398203561f1e865cc6c231.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ceea8dafd901dba7e2021da18dfad0",
+    "url": "/react-dnd-tutorial/index.html"
+  },
+  {
+    "revision": "cf6111138ebe4f20f09a",
+    "url": "/react-dnd-tutorial/static/js/2.54d853cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dnd-tutorial/static/js/2.54d853cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a711630d1b90c1efca2",
+    "url": "/react-dnd-tutorial/static/js/main.ba142d2d.chunk.js"
+  },
+  {
+    "revision": "b4a6eaf85541922c1f91",
+    "url": "/react-dnd-tutorial/static/js/runtime-main.8ee975c5.js"
+  }
+]);
